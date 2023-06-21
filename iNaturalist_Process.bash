dcefea45a392484159813734125429a3a9bccceb
MylesScholz <myles.scholz@gmail.com>
2023-06-21 10:39:18 -0700

    bash script changed to iteration.

--- iNaturalist_Process.bash
+++ iNaturalist_Process.bash
@@ -8,6 +8,11 @@ year="";
 totalArgs=$#;
 currentDate=`date`
 
+#List of sources to pull data from. These are the URL slugs from iNaturalist.org.
+#E.g., Oregon Bee Atlas URL: https://www.inaturalist.org/projects/oregon-bee-atlas-plant-images-sampleid
+#Oregon Bee Atlas URL slug: oregon-bee-atlas-plant-images-sampleid
+sources=("oregon-bee-atlas-plant-images-sampleid" "master-melittologist-outside-of-oregon");
+
 #Parse cmd line arguments
 i=1;
 while [ $i -le $((totalArgs - 1)) ]
@@ -21,57 +26,87 @@ do
     ((i++));
 done
 
-echo "Retreiving Oregon observations..."
+for source in "${sources}"; do
 
-#Run retrieval script for Oregon; return on success will be the filepath for format_data.py's input
-result=$(python iNaturalist_DataPull.py --year $year)
-#Print error if there was one
-if [ $? != 0 ]; then
-    echo "$result"
-    #Log error
-    echo "ERROR - $result - on $currentDate when retrieving Oregon observations" >> logFile.txt
-    exit $?
-fi
-echo "Retrieving Oregon observations => Done!"
+    echo "Retrieving '$source' observations..."
 
-echo "Formatting Oregon observations... (This may take a while)"
+    #Run retrieval script for source; return on success will be the filepath for format_data.py's input
+    result=$(python iNaturalist_DataPull.py --source $source --year $year)
+    #Print error if there was one
+    if [ $? != 0 ]; then
+        echo "$result"
+        #Log error
+        echo "ERROR - $result - on $currentDate when retrieving '$source' observations" >> logFile.txt
+        exit $?
+    fi
+    echo "Retrieving '$source' observations => Done!"
 
-#Run format script
-result=$(python format_data.py --input $result)
-#Print error if there was one
-if [ $? != 0 ]; then
-    echo "$result"
-    #Log error, leave out result as format_data.py prints many, many things
-    echo "ERROR - on $currentDate when formatting Oregon observations" >> logFile.txt
-    exit $?
-fi
-echo "Formatting Oregon observations => Done!"
+    echo "Formatting '$source' observations... (This may take a while)"
 
-echo "Retrieving observations outside Oregon..."
+    #Run format script
+    result=$(python format_data.py --input $result)
+    #Print error if there was one
+    if [ $? != 0 ]; then
+        echo "$result"
+        #Log error, leave out result as format_data.py prints many, many things
+        echo "ERROR - on $currentDate when formatting '$source' observations" >> logFile.txt
+        exit $?
+    fi
+    echo "Formatting '$source' observations => Done!"
 
-#Run retrieval script for outside Oregon; return on success will be the filepath for format_data.py's input
-result=$(python iNaturalist_DataPull.py --outside 1 --year $year)
-#Print error if there was one
-if [ $? != 0 ]; then
-    echo "$result"
-    #Log error
-    echo "ERROR - $result - on $currentDate when retrieving observations outside Oregon" >> logFile.txt
-    exit $?
-fi
-echo "Retrieving observations outside Oregon => Done!"
+done
 
-echo "Formatting observations outside Oregon... (This may take a while)"
+# echo "Retreiving Oregon observations..."
 
-#Run format script
-result=$(python format_data.py --input $result)
-#Print error if there was one
-if [ $? != 0 ]; then
-    echo "$result"
-    #Log error, leave out result as format_data.py prints many, many things
-    echo "ERROR - on $currentDate when formatting observations outside Oregon" >> logFile.txt
-    exit $?
-fi
-echo "Formatting observations outside Oregon => Done!"
+# #Run retrieval script for Oregon; return on success will be the filepath for format_data.py's input
+# result=$(python iNaturalist_DataPull.py --year $year)
+# #Print error if there was one
+# if [ $? != 0 ]; then
+#     echo "$result"
+#     #Log error
+#     echo "ERROR - $result - on $currentDate when retrieving Oregon observations" >> logFile.txt
+#     exit $?
+# fi
+# echo "Retrieving Oregon observations => Done!"
+
+# echo "Formatting Oregon observations... (This may take a while)"
+
+# #Run format script
+# result=$(python format_data.py --input $result)
+# #Print error if there was one
+# if [ $? != 0 ]; then
+#     echo "$result"
+#     #Log error, leave out result as format_data.py prints many, many things
+#     echo "ERROR - on $currentDate when formatting Oregon observations" >> logFile.txt
+#     exit $?
+# fi
+# echo "Formatting Oregon observations => Done!"
+
+# echo "Retrieving observations outside Oregon..."
+
+# #Run retrieval script for outside Oregon; return on success will be the filepath for format_data.py's input
+# result=$(python iNaturalist_DataPull.py --outside 1 --year $year)
+# #Print error if there was one
+# if [ $? != 0 ]; then
+#     echo "$result"
+#     #Log error
+#     echo "ERROR - $result - on $currentDate when retrieving observations outside Oregon" >> logFile.txt
+#     exit $?
+# fi
+# echo "Retrieving observations outside Oregon => Done!"
+
+# echo "Formatting observations outside Oregon... (This may take a while)"
+
+# #Run format script
+# result=$(python format_data.py --input $result)
+# #Print error if there was one
+# if [ $? != 0 ]; then
+#     echo "$result"
+#     #Log error, leave out result as format_data.py prints many, many things
+#     echo "ERROR - on $currentDate when formatting observations outside Oregon" >> logFile.txt
+#     exit $?
+# fi
+# echo "Formatting observations outside Oregon => Done!"
 
 #Log success
 echo "SUCCESS - Pulled and formatted data on $currentDate" >> logFile.txt
