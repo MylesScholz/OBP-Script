8c48d69a91d3c0492c13338970b5e03194e8274b
MylesScholz <myles.scholz@gmail.com>
2023-06-24 16:08:10 -0700

    Created merge_data.py. Debug mode of .bash.

---
+++ merge_data.py
@@ -0,0 +1,12 @@
+# Author: Myles Scholz
+# Created on June 24, 2023
+# Description: Merges formatted data (output from format_data.py)
+#              with existing data of the same format
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
